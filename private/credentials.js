f13e3678e66559ddf9eb2045393cc456232a6c58
gbuonc <gbuonc@gmail.com>
2015-10-12 14:07:48 +0000

    index+credentials

--- private/credentials.js
+++ private/credentials.js
@@ -5,6 +5,6 @@ module.exports = {
       email: '882927279330-cakgq3nhk79i56gulmdelo783pjrrmjf@developer.gserviceaccount.com',
       keyFile: './auth/auth.pem'
     },
-  spreadsheetId: '1QUq8v7RTUns3TTZgJdCS6V87lRvPN7rC23YIzWueBVA',
+  spreadsheetId: '0Anr77tcB9vJbdFhBUFRMNkliOWJXZDhqMlVOZ2FDdGc', //'1QUq8v7RTUns3TTZgJdCS6V87lRvPN7rC23YIzWueBVA',
   worksheetId: "od6"
 };
